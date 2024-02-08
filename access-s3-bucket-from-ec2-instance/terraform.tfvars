7d0c5f643e66dc44353acd621545e11c8e9e8774
serafino16 <40893656+serafino16@users.noreply.github.com>
2024-02-08 18:28:35 +0200

    Add files via upload

---
+++ access-s3-bucket-from-ec2-instance/terraform.tfvars
@@ -0,0 +1,3 @@
+access_key = "AKIA562SCJAKXPFLAHVI"
+secret_key = "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"
+bucket_name = "my_bucket_course123421"
---
+++ access-s3-bucket-from-ec2-instance/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  default = "us-east-1"
+}
+variable "secret_key" {
+  
+}
+variable "access_key" {
+  
+}
+variable "bucket_name" {
+  
+}
